8bf494c566c3a5d857b17dd32b4875f6c008295a
heosman <77591083+heosman@users.noreply.github.com>
2021-12-05 22:44:34 -0800

    Updating app styles

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 function myFunction1(){ 
-    document.getElementById("display").innerHTML="Hello World";
+    document.getElementById("display").innerHTML="Hello, World!";
+    document.getElementById("display").setAttribute("class", "display");
 }
 
 function myFunction2(){
-    document.getElementById("display").innerHTML="Goodbye World";
+    document.getElementById("display").innerHTML="Goodbye, Cruel World!";
+    document.getElementById("display").setAttribute("class", "display");
 }
