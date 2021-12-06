0618b88cb7c9a0ad1d0671e8a06d9a051ea1dbdd
heosman <77591083+heosman@users.noreply.github.com>
2021-12-05 16:43:53 -0800

    JavaScript file

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function myFunction1(){ 
+    document.getElementById("display").innerHTML="Hello World";
+}
+function myFunction2(){
+    document.getElementById("display").innerHTML="Goodbye World";
+}
